9afae9fecd30faf79f8b9eca96ad58b77d98696a
Anant Kanchan <anant4265@gmail.com>
2021-02-20 22:38:24 +0530

    Variables declared and initialized

--- app/src/main/java/com/anant/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/anant/bmicalculator/MainActivity.kt
@@ -2,10 +2,20 @@ package com.anant.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var etHeight:EditText
+    lateinit var etWeight:EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etHeight=findViewById(R.id.et_height)
+        etWeight=findViewById(R.id.et_weight)
+
+
     }
 }
