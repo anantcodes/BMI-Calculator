9d6ff9fc1f105eaa28c5b168d77f060b3d4c7349
Anant Kanchan <anant4265@gmail.com>
2021-02-21 18:53:34 +0530

    control_activity UI enhanced

--- app/src/main/java/com/anant/bmicalculator/ControlActivity.kt
+++ app/src/main/java/com/anant/bmicalculator/ControlActivity.kt
@@ -1,11 +1,33 @@
 package com.anant.bmicalculator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ControlActivity : AppCompatActivity() {
+
+    lateinit var btnBmi:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_control)
+
+        btnBmi=findViewById(R.id.btn_bmi)
+        btnBmi.setOnClickListener {
+            startActivity(
+                Intent(this,
+                    MainActivity::class.java)
+            )
+
+        }
+
+
+
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/anant/bmicalculator/SplashActivity.kt
+++ app/src/main/java/com/anant/bmicalculator/SplashActivity.kt
@@ -13,7 +13,7 @@ class SplashActivity : AppCompatActivity() {
         Handler().postDelayed({
             startActivity(
                     Intent(this,
-                            MainActivity::class.java)
+                            ControlActivity::class.java)
             )
             finish()
         }, splashTime)
